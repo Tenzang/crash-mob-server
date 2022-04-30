7875076fa3625655d2866fb0b8622b9831e652f6
Jolly Yau <josephhu706@hotmail.com>
2022-04-30 12:50:44 +1000

    merged conflicts in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root to: "static#home"
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
-=======
   get 'users/new'
-
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
   resources :characters, :only => [:new, :create, :update, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 633a41910719851bdaac7b6d8586d949912a08d3
 end
