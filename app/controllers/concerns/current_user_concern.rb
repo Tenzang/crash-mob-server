c26721b4cbc547f32a9a63d15ecfb79130e1b487
Jolly Yau <josephhu706@hotmail.com>
2022-05-01 15:50:58 +1000

    fixed sessions bug

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,11 +2,15 @@ module CurrentUserConcern
     extend ActiveSupport::Concern
 
     included do
+        p 'current user being set'
         before_action :set_current_user
     end
 
     def set_current_user
+        p '/////////////////////////////////////'
         return @current_user if defined?(@current_user)
-        @current_user = User.find_by(id: session[:id])
+        p session[:id]
+        @current_user = User.find_by(id: session[:user_id])
+        p @current_user
     end
 end
